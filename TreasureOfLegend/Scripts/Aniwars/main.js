2ae0300f30fc974d24b411dd6c941f5b9668e349
Tavi <octavian.patraulea@softvision.ro>
2019-01-16 14:36:50 +0200

    tidy up

--- TreasureOfLegend/Scripts/Aniwars/main.js
+++ TreasureOfLegend/Scripts/Aniwars/main.js
@@ -34,6 +34,11 @@ export const AniwarsGame = function () {
         create() {
             this.activeMap = new BattleMap(this);
             this.activeMap.generateMap();
+            var self = this;
+            _.each(this.activeMap.tiles.getChildren(), function(tile) {
+                tile.on('pointerdown', _.bind(self._moveCharacterOnClick, self, tile));
+                tile.on('pointerover', _.bind(self._highlightPathToThis, self, tile));
+            });
 
             //party characters
             this.characters = new Character(this, this.add.group());
@@ -45,28 +50,44 @@ export const AniwarsGame = function () {
             //enemy characters
             this.enemies = new Character(this, this.add.group());
             this.enemies.addNewCharacter(1100, 350, 'character');
-            this.input.on('gameobjectdown', _.bind(this._moveCharacterOnClick, this));
-            this.input.on('pointerover', _.bind(this._highlightPathToThis, this));
 
+            //mouse input on clicking game objects and hovering over them
+            //this.input.on('gameobjectdown', _.bind(this._moveCharacterOnClick, this));
+            //this.input.on('pointerover', _.bind(this._highlightPathToThis, this));
+
+            //text with current character current location
             this.locationText = this.add.text(10, 10, 'X: 0, Y: 0', { fill: '#0f0' });
+
+            //main camera
+            this.cameras.main.setBounds(0, 0, this.activeMap.levelMap.length * 50, this.activeMap.levelMap.length * 50 + 100);
+            this.cameras.main.startFollow(this.activeCharacter, true, 0.09, 0.09);
+
+            //create player hud
+            //var rect = new Phaser.Geom.Rectangle(0, 700, 1200, 800);
+            //this.hud = this.add.graphics({ fillStyle: { color: 0x0000ff } });
+            //this.hud.fillRectShape(rect);
+            //graphics.setInteractive(rect, event);
         },
         update() {
             this._checkManager();
         },
-        _moveCharacterOnClick(pointer, tile) {
-            this.characters.moveActiveCharacter(tile, { enemies: this.enemies});
-        },
         _checkManager() {
             this.locationText.setText('X: '+ Math.floor(this.activeCharacter.x / 50) + ', Y: ' + Math.floor(this.activeCharacter.y / 50));
+            this.locationText.setX(this.cameras.main.scrollX + 10);
+            this.locationText.setY(this.cameras.main.scrollY + 10);
+            //this.hud.setX(this.cameras.main.scrollX);
+            //this.hud.setY(this.cameras.main.scrollY + 700);
             if (this.activeCharacter.characterConfig.isMoving) {
                 this.characters.stopActiveCharacter();
             } else if (!this.activeCharacter.characterConfig.isMoving && this.activeCharacter.characterConfig.path.length > 0) {
                 this.characters.keepMovingActiveCharacter();
             }
         },
-        
-        _highlightPathToThis(pointer, tiles) {
-            this.activeMap.highlightPathToThis(tiles[0]);
+        _moveCharacterOnClick(tile) {
+            this.characters.moveActiveCharacter(tile, { enemies: this.enemies});
+        },
+        _highlightPathToThis(tile) {
+            this.activeMap.highlightPathToThis(tile);
         }
     });
 
